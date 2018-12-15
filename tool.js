c0c98492e8d0b853a29bfb8c72557609acffdd49
Gokul Sadanandam <gokul.sadanandam@tatacommunications.com>
2018-12-16 00:22:56 +0530

    Added Synonyms and Antonyms Logics! Added Parsing Logic!

--- tool.js
+++ tool.js
@@ -3,30 +3,49 @@ let express = require('express'),
     request = require('request')
 const readline = require('readline');
 
-
 const app = {
-      parseInput : function(){
+    parseInput: function() {
         this.type = process.argv[2]
         this.word = process.argv[3]
-        switch (this.type){
-          case "def":
-              this.getDefinition(this.word)
-              break
-          default:
-            console.log("Invalid Command")
-            break
+        switch (this.type) {
+            case "def":
+                this.getWordDetail(this.word,"synonyms")
+                break;
+            case "ant":
+                this.getWordDetail(this.word,"antonyms")
+                break;
+            default:
+                console.log("Invalid Command")
+                break;
         }
-      },
-      getDefinition : function(word){
-        console.log("Getting Definition")
-      }
+    },
+    parseJSONData : function(input,parameter){
+        Object.keys(input).forEach((data)=>{
+          if(parameter==data){
+            input[data].forEach((values)=>{
+              console.log("\t" + values.text)
+            })
+              return input[data]
+          }else if(input[data] instanceof Array){
+              this.parseJSONData(input[data][0],parameter)
+            }
+        })
+    },
+    getWordDetail: function(word,value) {
+        const options = {
+            "url": 'https://od-api.oxforddictionaries.com/api/v1/entries/en/' + word + '/' + value,
+            "headers": {
+                "Accept": "application/json",
+                "app_id": "aa63a5d1",
+                "app_key": "63a40f1f9cd31d55134b61c79b44d471",
+            }
+        };
 
+        request.get(options, (err, resp, body) => {
 
-
-        }
-    
-
-
-
+            let data =  this.parseJSONData((JSON.parse(body)),value)
+        })
+    }
+}
 
 module.exports = app.parseInput()
